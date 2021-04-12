7a26afbacebf3ff5b4a559c423fc277f256ec96e
pangwa <pangwa@gmail.com>
2021-04-12 12:37:47 +0800

    fee handling on chain
    fees are charged on chain for mint/burn transactions

--- modules/claims/src/lib.rs
+++ modules/claims/src/lib.rs
@@ -4,23 +4,16 @@
 //! Module to process claims from ethereum like addresses(e.g. bsc).
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use sp_std::{
-  prelude::*,
-};
-use frame_support::{
-  traits::{Currency, Get, WithdrawReasons, ExistenceRequirement, }
-};
-use frame_system::{ensure_signed};
-use codec::{Encode, };
+use codec::Encode;
+use frame_support::traits::{Currency, ExistenceRequirement, Get, WithdrawReasons};
+use frame_system::ensure_signed;
+use sp_io::{crypto::secp256k1_ecdsa_recover, hashing::keccak_256};
 use sp_runtime::{
-  traits::{
-    Saturating
-  },
-  transaction_validity::{
-    ValidTransaction, InvalidTransaction, TransactionValidity,
-  },
+  traits::{AccountIdConversion, Saturating},
+  transaction_validity::{InvalidTransaction, TransactionValidity, ValidTransaction},
+  ModuleId,
 };
-use sp_io::{hashing::keccak_256, crypto::secp256k1_ecdsa_recover};
+use sp_std::prelude::*;
 
 pub use pallet::*;
 pub mod ethereum_address;
@@ -39,10 +32,12 @@ pub mod pallet {
   use frame_support::pallet_prelude::*;
   use frame_system::pallet_prelude::*;
 
-  pub type BalanceOf<T> = <<T as Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
+  pub type BalanceOf<T> =
+    <<T as Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
 
   #[pallet::config]
   pub trait Config: frame_system::Config {
+    type ModuleId: Get<ModuleId>;
     type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
     type Currency: Currency<Self::AccountId>;
     type Prefix: Get<&'static [u8]>;
@@ -52,8 +47,7 @@ pub mod pallet {
   pub struct Pallet<T>(sp_std::marker::PhantomData<T>);
 
   #[pallet::hooks]
-  impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
-  }
+  impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
 
   #[pallet::error]
   pub enum Error<T> {
@@ -64,6 +58,7 @@ pub mod pallet {
     TxNotMinted,
     InvalidEthereumSignature,
     SignatureNotMatch,
+    InvalidAmount,
   }
 
   #[pallet::event]
@@ -80,6 +75,9 @@ pub mod pallet {
     Claimed(T::AccountId, EthereumAddress, BalanceOf<T>),
     // burned some balance and will bridge to bsc
     Burned(T::AccountId, EthereumAddress, BalanceOf<T>),
+
+    MintFeeUpdated(BalanceOf<T>),
+    BurnFeeUpdated(BalanceOf<T>),
   }
 
   #[pallet::storage]
@@ -92,14 +90,27 @@ pub mod pallet {
 
   #[pallet::storage]
   #[pallet::getter(fn claims)]
-  pub(super) type Claims<T: Config> = StorageMap<_, Blake2_128Concat, EthereumTxHash, Option<(EthereumAddress, BalanceOf<T>, bool)>, ValueQuery>;
+  pub(super) type Claims<T: Config> = StorageMap<
+    _,
+    Blake2_128Concat,
+    EthereumTxHash,
+    Option<(EthereumAddress, BalanceOf<T>, bool)>,
+    ValueQuery,
+  >;
+
+  #[pallet::storage]
+  #[pallet::getter(fn mint_fee)]
+  pub(super) type MintFee<T: Config> = StorageValue<_, Option<BalanceOf<T>>, ValueQuery>;
+
+  #[pallet::storage]
+  #[pallet::getter(fn burn_fee)]
+  pub(super) type BurnFee<T: Config> = StorageValue<_, Option<BalanceOf<T>>, ValueQuery>;
 
   #[pallet::call]
   impl<T: Config> Pallet<T> {
-
     #[pallet::weight(T::DbWeight::get().writes(2))]
     #[frame_support::transactional]
-    pub(super) fn set_bridge_account (
+    pub(super) fn set_bridge_account(
       origin: OriginFor<T>,
       to: T::AccountId,
     ) -> DispatchResultWithPostInfo {
@@ -114,7 +125,10 @@ pub mod pallet {
 
     #[pallet::weight(T::DbWeight::get().writes(2))]
     #[frame_support::transactional]
-    pub fn set_claim_limit(origin: OriginFor<T>, limit: BalanceOf<T>) -> DispatchResultWithPostInfo {
+    pub fn set_claim_limit(
+      origin: OriginFor<T>,
+      limit: BalanceOf<T>,
+    ) -> DispatchResultWithPostInfo {
       ensure_root(origin)?;
 
       ClaimLimit::<T>::put(limit.clone());
@@ -123,31 +137,73 @@ pub mod pallet {
       Ok(().into())
     }
 
+    #[pallet::weight(T::DbWeight::get().writes(1))]
+    #[frame_support::transactional]
+    pub fn set_mint_fee(origin: OriginFor<T>, fee: BalanceOf<T>) -> DispatchResultWithPostInfo {
+      ensure_root(origin)?;
+
+      MintFee::<T>::put(Some(fee.clone()));
+      Self::deposit_event(Event::MintFeeUpdated(fee));
+      Ok(().into())
+    }
+
+    #[pallet::weight(T::DbWeight::get().writes(1))]
+    #[frame_support::transactional]
+    pub fn set_burn_fee(origin: OriginFor<T>, fee: BalanceOf<T>) -> DispatchResultWithPostInfo {
+      ensure_root(origin)?;
+
+      BurnFee::<T>::put(Some(fee.clone()));
+      Self::deposit_event(Event::BurnFeeUpdated(fee));
+      Ok(().into())
+    }
+
     #[pallet::weight(T::DbWeight::get().writes(3))]
     #[frame_support::transactional]
-    pub fn mint_claim(origin: OriginFor<T>, tx: EthereumTxHash, who: EthereumAddress, value: BalanceOf<T>)
-        -> DispatchResultWithPostInfo {
-        let signer = ensure_signed(origin)?;
-        let bridge_account = Self::bridge_account();
+    pub fn mint_claim(
+      origin: OriginFor<T>,
+      tx: EthereumTxHash,
+      who: EthereumAddress,
+      value: BalanceOf<T>,
+    ) -> DispatchResultWithPostInfo {
+      let signer = ensure_signed(origin)?;
+      let bridge_account = Self::bridge_account();
 
-        // mint must be orginated from bridge account
-        ensure!(Some(&signer) == bridge_account.as_ref(), Error::<T>::NoPermission);
-        // Check if this tx already be mint or be claimed
-        ensure!(!Claims::<T>::contains_key(&tx), Error::<T>::AlreadyMinted);
-        // Check claim limit
-        ensure!(Self::claim_limit() >= value, Error::<T>::ClaimLimitExceeded);
-        // insert into claims
-        Claims::<T>::insert(tx.clone(), Some((who.clone(), value.clone(), false)));
-        // update claim limit
-        ClaimLimit::<T>::mutate(|l| *l = l.saturating_sub(value));
+      // mint must be orginated from bridge account
+      ensure!(
+        Some(&signer) == bridge_account.as_ref(),
+        Error::<T>::NoPermission
+      );
+      // Check if this tx already be mint or be claimed
+      ensure!(!Claims::<T>::contains_key(&tx), Error::<T>::AlreadyMinted);
+      // Check claim limit
+      ensure!(Self::claim_limit() >= value, Error::<T>::ClaimLimitExceeded);
+      let mut claim_amount = value.clone();
+      let mut mint_fee = 0u32.into();
+      if let Some(fee) = Self::mint_fee() {
+        ensure!(value > fee, Error::<T>::InvalidAmount);
+        claim_amount = value.saturating_sub(fee);
+        mint_fee = fee;
+      }
+      // insert into claims
+      Claims::<T>::insert(tx.clone(), Some((who.clone(), claim_amount.clone(), false)));
+      // update claim limit
+      ClaimLimit::<T>::mutate(|l| *l = l.saturating_sub(claim_amount));
+      if mint_fee > 0u32.into() {
+        T::Currency::deposit_creating(&Self::account_id(), mint_fee);
+      }
 
-        Self::deposit_event(Event::MintSuccess(tx, who, value));
-        Ok(().into())
+      Self::deposit_event(Event::MintSuccess(tx, who, claim_amount));
+      Ok(().into())
     }
 
     #[pallet::weight(0)]
     #[frame_support::transactional]
-    pub fn claim(origin: OriginFor<T>, dest: T::AccountId, tx: EthereumTxHash, sig: EcdsaSignature) -> DispatchResultWithPostInfo {
+    pub fn claim(
+      origin: OriginFor<T>,
+      dest: T::AccountId,
+      tx: EthereumTxHash,
+      sig: EcdsaSignature,
+    ) -> DispatchResultWithPostInfo {
       ensure_none(origin)?;
       let tx_info = Self::claims(&tx);
       ensure!(tx_info.is_some(), Error::<T>::TxNotMinted);
@@ -157,7 +213,8 @@ pub mod pallet {
 
       let data = dest.using_encoded(to_ascii_hex);
 
-      let signer = Self::eth_recover(&sig, &data, &[][..]).ok_or(Error::<T>::InvalidEthereumSignature)?;
+      let signer =
+        Self::eth_recover(&sig, &data, &[][..]).ok_or(Error::<T>::InvalidEthereumSignature)?;
 
       ensure!(address == signer, Error::<T>::SignatureNotMatch);
 
@@ -171,11 +228,31 @@ pub mod pallet {
 
     #[pallet::weight(T::DbWeight::get().reads_writes(2, 3))]
     #[frame_support::transactional]
-    pub fn burn(origin: OriginFor<T>, dest: EthereumAddress, amount: BalanceOf<T>) -> DispatchResultWithPostInfo {
+    pub fn burn(
+      origin: OriginFor<T>,
+      dest: EthereumAddress,
+      amount: BalanceOf<T>,
+    ) -> DispatchResultWithPostInfo {
       let who = ensure_signed(origin)?;
-      T::Currency::withdraw(&who, amount, WithdrawReasons::TRANSFER, ExistenceRequirement::KeepAlive)?;
+      let mut burn_amount = amount.clone();
+      let mut burn_fee = 0u32.into();
+      if let Some(fee) = Self::burn_fee() {
+        ensure!(amount > fee, Error::<T>::InvalidAmount);
+        burn_amount = amount.saturating_sub(fee);
+        burn_fee = fee;
+      }
 
-      Self::deposit_event(Event::Burned(who, dest, amount));
+      T::Currency::withdraw(
+        &who,
+        amount,
+        WithdrawReasons::TRANSFER,
+        ExistenceRequirement::KeepAlive,
+      )?;
+      if burn_fee > 0u32.into() {
+        T::Currency::deposit_creating(&Self::account_id(), burn_fee);
+      }
+
+      Self::deposit_event(Event::Burned(who, dest, burn_amount));
       Ok(().into())
     }
   }
@@ -194,24 +271,22 @@ pub mod pallet {
 
   impl From<ValidityError> for u8 {
     fn from(err: ValidityError) -> Self {
-        err as u8
+      err as u8
     }
   }
 
   #[pallet::validate_unsigned]
-	impl<T: Config> ValidateUnsigned for Pallet<T> {
-		type Call = Call<T>;
+  impl<T: Config> ValidateUnsigned for Pallet<T> {
+    type Call = Call<T>;
 
-		fn validate_unsigned(
-			_source: TransactionSource,
-			call: &Self::Call,
-		) -> TransactionValidity {
+    fn validate_unsigned(_source: TransactionSource, call: &Self::Call) -> TransactionValidity {
       const PRIORITY: u64 = 100;
 
-			if let Call::claim(account, tx, sig)  = call {
+      if let Call::claim(account, tx, sig) = call {
         let data = account.using_encoded(to_ascii_hex);
-        let signer = Self::eth_recover(&sig, &data, &[][..])
-          .ok_or(InvalidTransaction::Custom(ValidityError::InvalidEthereumSignature.into()))?;
+        let signer = Self::eth_recover(&sig, &data, &[][..]).ok_or(InvalidTransaction::Custom(
+          ValidityError::InvalidEthereumSignature.into(),
+        ))?;
 
         let e = InvalidTransaction::Custom(ValidityError::SignerHasNoClaim.into());
         let tx_info = Self::claims(&tx);
@@ -230,30 +305,34 @@ pub mod pallet {
           provides: vec![("claims", signer).encode()],
           longevity: TransactionLongevity::max_value(),
           propagate: true,
-       })
-			} else {
-				InvalidTransaction::Call.into()
-			}
-		}
-	}
+        })
+      } else {
+        InvalidTransaction::Call.into()
+      }
+    }
+  }
 
-  impl <T: Config> Pallet<T> {
+  impl<T: Config> Pallet<T> {
+    /// the account to store the mint/claim fees
+    pub fn account_id() -> T::AccountId {
+      T::ModuleId::get().into_account()
+    }
 
     // Constructs the message that Ethereum RPC's `personal_sign` and `eth_sign` would sign.
     fn ethereum_signable_message(what: &[u8], extra: &[u8]) -> Vec<u8> {
-       let prefix = T::Prefix::get();
-       let mut l = prefix.len() + what.len() + extra.len();
-       let mut rev = Vec::new();
-       while l > 0 {
-           rev.push(b'0' + (l % 10) as u8);
-           l /= 10;
-       }
-       let mut v = b"\x19Ethereum Signed Message:\n".to_vec();
-       v.extend(rev.into_iter().rev());
-       v.extend_from_slice(&prefix[..]);
-       v.extend_from_slice(what);
-       v.extend_from_slice(extra);
-       v
+      let prefix = T::Prefix::get();
+      let mut l = prefix.len() + what.len() + extra.len();
+      let mut rev = Vec::new();
+      while l > 0 {
+        rev.push(b'0' + (l % 10) as u8);
+        l /= 10;
+      }
+      let mut v = b"\x19Ethereum Signed Message:\n".to_vec();
+      v.extend(rev.into_iter().rev());
+      v.extend_from_slice(&prefix[..]);
+      v.extend_from_slice(what);
+      v.extend_from_slice(extra);
+      v
     }
 
     // Attempts to recover the Ethereum address from a message signature signed by using
@@ -261,7 +340,9 @@ pub mod pallet {
     fn eth_recover(s: &EcdsaSignature, what: &[u8], extra: &[u8]) -> Option<EthereumAddress> {
       let msg = keccak_256(&Self::ethereum_signable_message(what, extra));
       let mut res = EthereumAddress::default();
-      res.0.copy_from_slice(&keccak_256(&secp256k1_ecdsa_recover(&s.0, &msg).ok()?[..])[12..]);
+      res
+        .0
+        .copy_from_slice(&keccak_256(&secp256k1_ecdsa_recover(&s.0, &msg).ok()?[..])[12..]);
       Some(res)
     }
   }
--- modules/claims/src/tests.rs
+++ modules/claims/src/tests.rs
@@ -7,192 +7,305 @@ use frame_support::{assert_noop, assert_ok, dispatch::DispatchError};
 use pallet_balances::Error as BalancesError;
 
 #[test]
-fn mint_and_claim_should_works () {
-    new_test_ext().execute_with(|| {
-        // set bridge account to root
-        assert_ok!(CloverClaims::set_bridge_account(Origin::root(), 1));
+fn mint_and_claim_should_works() {
+  new_test_ext().execute_with(|| {
+    // set bridge account to root
+    assert_ok!(CloverClaims::set_bridge_account(Origin::root(), 1));
 
-        // Set claim limit = 100
-        assert_ok!(CloverClaims::set_claim_limit(Origin::root(), 100));
-        assert_eq!(CloverClaims::claim_limit(), 100);
+    // Set claim limit = 100
+    assert_ok!(CloverClaims::set_claim_limit(Origin::root(), 100));
+    assert_eq!(CloverClaims::claim_limit(), 100);
 
-        // Mint a claim
-        let tx_hash = get_legal_tx_hash();
-        let eth_addr = get_legal_eth_addr();
-        assert_ok!(CloverClaims::mint_claim(Origin::signed(1), tx_hash.clone(), eth_addr.clone(), 100));
+    // Mint a claim
+    let tx_hash = get_legal_tx_hash();
+    let eth_addr = get_legal_eth_addr();
+    assert_ok!(CloverClaims::mint_claim(
+      Origin::signed(1),
+      tx_hash.clone(),
+      eth_addr.clone(),
+      100
+    ));
 
-        // claims and limit should updated
-        assert_eq!(CloverClaims::claims(&tx_hash), Some((eth_addr.clone(), 100, false))); // new tx
-        assert_eq!(CloverClaims::claim_limit(), 0);
+    // claims and limit should updated
+    assert_eq!(
+      CloverClaims::claims(&tx_hash),
+      Some((eth_addr.clone(), 100, false))
+    ); // new tx
+    assert_eq!(CloverClaims::claim_limit(), 0);
 
-        // Claim it with correct msg sig
-        // Pay CLVs to the TEST account:0100000000000000
-        let sig = get_legal_eth_sig();
-        assert_eq!(Balances::free_balance(1), 0);
-        assert_ok!(CloverClaims::claim(Origin::none(), 1, tx_hash.clone(), sig.clone()));
+    // Claim it with correct msg sig
+    // Pay CLVs to the TEST account:0100000000000000
+    let sig = get_legal_eth_sig();
+    assert_eq!(Balances::free_balance(1), 0);
+    assert_ok!(CloverClaims::claim(
+      Origin::none(),
+      1,
+      tx_hash.clone(),
+      sig.clone()
+    ));
 
-        // 5. Claim success
-        assert_eq!(Balances::free_balance(1), 100);
-        assert_eq!(CloverClaims::claims(&tx_hash), Some((eth_addr.clone(), 100, true))); // tx has already be claimed
-    });
+    // 5. Claim success
+    assert_eq!(Balances::free_balance(1), 100);
+    assert_eq!(
+      CloverClaims::claims(&tx_hash),
+      Some((eth_addr.clone(), 100, true))
+    ); // tx has already be claimed
+  });
+}
+
+#[test]
+fn mint_fee_should_work() {
+  new_test_ext().execute_with(|| {
+    assert_ok!(CloverClaims::set_mint_fee(Origin::root(), 1000));
+    // set bridge account to root
+    assert_ok!(CloverClaims::set_bridge_account(Origin::root(), 1));
+    assert_ok!(CloverClaims::set_claim_limit(Origin::root(), 2000));
+
+    // Mint a claim
+    let tx_hash = get_legal_tx_hash();
+    let eth_addr = get_legal_eth_addr();
+    assert_noop!(
+      CloverClaims::mint_claim(Origin::signed(1), tx_hash.clone(), eth_addr.clone(), 100),
+      Error::<Test>::InvalidAmount
+    );
+
+    assert_eq!(CloverClaims::claim_limit(), 2000);
+
+    assert_ok!(CloverClaims::mint_claim(
+      Origin::signed(1),
+      tx_hash.clone(),
+      eth_addr.clone(),
+      1200
+    ));
+
+    // claim limit only update with the real amount
+    assert_eq!(CloverClaims::claim_limit(), 1800);
+    // Claim it with correct msg sig
+    // Pay CLVs to the TEST account:0100000000000000
+    let sig = get_legal_eth_sig();
+    assert_eq!(Balances::free_balance(1), 0);
+    assert_ok!(CloverClaims::claim(
+      Origin::none(),
+      1,
+      tx_hash.clone(),
+      sig.clone()
+    ));
+
+    // 5. Claim success
+    assert_eq!(Balances::free_balance(1), 200);
+
+    // module has the fee deposited
+    assert_eq!(Balances::free_balance(CloverClaims::account_id()), 1000);
+  })
 }
 
 #[test]
 fn change_bridger_should_work() {
-    new_test_ext().execute_with(|| {
-        assert_noop!(
-            CloverClaims::set_bridge_account(Origin::signed(1), 1),
-            DispatchError::BadOrigin
-        );
+  new_test_ext().execute_with(|| {
+    assert_noop!(
+      CloverClaims::set_bridge_account(Origin::signed(1), 1),
+      DispatchError::BadOrigin
+    );
 
-        // set bridger
-        assert_ok!(CloverClaims::set_bridge_account(Origin::root(), 1)); // 1 is bridger
+    // set bridger
+    assert_ok!(CloverClaims::set_bridge_account(Origin::root(), 1)); // 1 is bridger
 
-        // 1. Mint a claim with 2, no way
-        let tx_hash = get_legal_tx_hash();
-        let eth_addr = get_legal_eth_addr();
-        assert_noop!(
-            CloverClaims::mint_claim(Origin::signed(2), tx_hash.clone(), eth_addr.clone(), 100),
-            Error::<Test>::NoPermission
-        );
-    });
+    // 1. Mint a claim with 2, no way
+    let tx_hash = get_legal_tx_hash();
+    let eth_addr = get_legal_eth_addr();
+    assert_noop!(
+      CloverClaims::mint_claim(Origin::signed(2), tx_hash.clone(), eth_addr.clone(), 100),
+      Error::<Test>::NoPermission
+    );
+  });
 }
 
 #[test]
 fn non_exist_tx_should_fail() {
-    new_test_ext().execute_with(|| {
-        let tx_hash = get_legal_tx_hash();
-        let sig = get_legal_eth_sig();
-        assert_noop!(
-            CloverClaims::claim(Origin::none(), 1, tx_hash, sig),
-            Error::<Test>::TxNotMinted
-        );
-    });
+  new_test_ext().execute_with(|| {
+    let tx_hash = get_legal_tx_hash();
+    let sig = get_legal_eth_sig();
+    assert_noop!(
+      CloverClaims::claim(Origin::none(), 1, tx_hash, sig),
+      Error::<Test>::TxNotMinted
+    );
+  });
 }
 
 #[test]
 fn illegal_sig_claim_should_fail() {
-    new_test_ext().execute_with(|| {
-        // only root account can change bridge account
-        assert_noop!(
-          CloverClaims::set_bridge_account(Origin::signed(2), 1),
-          DispatchError::BadOrigin
-        );
-        assert_ok!(CloverClaims::set_bridge_account(Origin::root(), 1));
+  new_test_ext().execute_with(|| {
+    // only root account can change bridge account
+    assert_noop!(
+      CloverClaims::set_bridge_account(Origin::signed(2), 1),
+      DispatchError::BadOrigin
+    );
+    assert_ok!(CloverClaims::set_bridge_account(Origin::root(), 1));
 
-        assert_ok!(CloverClaims::set_claim_limit(Origin::root(), 100));
+    assert_ok!(CloverClaims::set_claim_limit(Origin::root(), 100));
 
-        //  Mint a claim
-        let tx_hash = get_legal_tx_hash();
-        let eth_addr = get_legal_eth_addr();
-        assert_ok!(CloverClaims::mint_claim(Origin::signed(1), tx_hash.clone(), eth_addr.clone(), 100));
+    //  Mint a claim
+    let tx_hash = get_legal_tx_hash();
+    let eth_addr = get_legal_eth_addr();
+    assert_ok!(CloverClaims::mint_claim(
+      Origin::signed(1),
+      tx_hash.clone(),
+      eth_addr.clone(),
+      100
+    ));
 
-        // Claim it with illegal sig
-        let sig1 = get_another_account_eth_sig();
-        assert_noop!(
-            CloverClaims::claim(Origin::none(), 1, tx_hash.clone(), sig1.clone()),
-            Error::<Test>::SignatureNotMatch
-        );
+    // Claim it with illegal sig
+    let sig1 = get_another_account_eth_sig();
+    assert_noop!(
+      CloverClaims::claim(Origin::none(), 1, tx_hash.clone(), sig1.clone()),
+      Error::<Test>::SignatureNotMatch
+    );
 
-        // Sig with wrong message should failed
-        let sig2 = get_wrong_msg_eth_sig();
-        assert_noop!(
-            CloverClaims::claim(Origin::none(), 1, tx_hash.clone(), sig2.clone()),
-            Error::<Test>::SignatureNotMatch
-        );
-    });
+    // Sig with wrong message should failed
+    let sig2 = get_wrong_msg_eth_sig();
+    assert_noop!(
+      CloverClaims::claim(Origin::none(), 1, tx_hash.clone(), sig2.clone()),
+      Error::<Test>::SignatureNotMatch
+    );
+  });
 }
 
 #[test]
 fn mint_twice_should_fail() {
-    new_test_ext().execute_with(|| {
-        // Set miner and superior
-        assert_ok!(CloverClaims::set_bridge_account(Origin::root(), 1));
+  new_test_ext().execute_with(|| {
+    // Set miner and superior
+    assert_ok!(CloverClaims::set_bridge_account(Origin::root(), 1));
 
-        // Set limit
-        assert_ok!(CloverClaims::set_claim_limit(Origin::root(), 100));
+    // Set limit
+    assert_ok!(CloverClaims::set_claim_limit(Origin::root(), 100));
 
-        // Mint a claim
-        let tx_hash = get_legal_tx_hash();
-        let eth_addr = get_legal_eth_addr();
-        assert_ok!(CloverClaims::mint_claim(Origin::signed(1), tx_hash.clone(), eth_addr.clone(), 100));
+    // Mint a claim
+    let tx_hash = get_legal_tx_hash();
+    let eth_addr = get_legal_eth_addr();
+    assert_ok!(CloverClaims::mint_claim(
+      Origin::signed(1),
+      tx_hash.clone(),
+      eth_addr.clone(),
+      100
+    ));
 
-        // 3. Mint the same eth again
-        assert_noop!(
-            CloverClaims::mint_claim(Origin::signed(1), tx_hash.clone(), eth_addr.clone(), 100),
-            Error::<Test>::AlreadyMinted
-        );
-    });
+    // 3. Mint the same eth again
+    assert_noop!(
+      CloverClaims::mint_claim(Origin::signed(1), tx_hash.clone(), eth_addr.clone(), 100),
+      Error::<Test>::AlreadyMinted
+    );
+  });
 }
 
 #[test]
 fn claim_twice_should_fail() {
-    new_test_ext().execute_with(|| {
-        assert_ok!(CloverClaims::set_bridge_account(Origin::root(), 1));
+  new_test_ext().execute_with(|| {
+    assert_ok!(CloverClaims::set_bridge_account(Origin::root(), 1));
 
-        // Set limit
-        assert_ok!(CloverClaims::set_claim_limit(Origin::root(), 100));
+    // Set limit
+    assert_ok!(CloverClaims::set_claim_limit(Origin::root(), 100));
 
-        // Mint a claim
-        let tx_hash = get_legal_tx_hash();
-        let eth_addr = get_legal_eth_addr();
-        assert_ok!(CloverClaims::mint_claim(Origin::signed(1), tx_hash.clone(), eth_addr.clone(), 100));
+    // Mint a claim
+    let tx_hash = get_legal_tx_hash();
+    let eth_addr = get_legal_eth_addr();
+    assert_ok!(CloverClaims::mint_claim(
+      Origin::signed(1),
+      tx_hash.clone(),
+      eth_addr.clone(),
+      100
+    ));
 
-        // Claim it
-        // Pay CLVs to the TEST account:0100000000000000
-        let sig = get_legal_eth_sig();
-        assert_eq!(Balances::free_balance(1), 0);
-        assert_ok!(CloverClaims::claim(Origin::none(), 1, tx_hash.clone(), sig.clone()));
-        assert_eq!(Balances::free_balance(1), 100);
+    // Claim it
+    // Pay CLVs to the TEST account:0100000000000000
+    let sig = get_legal_eth_sig();
+    assert_eq!(Balances::free_balance(1), 0);
+    assert_ok!(CloverClaims::claim(
+      Origin::none(),
+      1,
+      tx_hash.clone(),
+      sig.clone()
+    ));
+    assert_eq!(Balances::free_balance(1), 100);
 
-        // failed on the second try
-        assert_noop!(
-            CloverClaims::claim(Origin::none(), 1, tx_hash.clone(), sig.clone()),
-            Error::<Test>::AlreadyClaimed
-        );
-        assert_eq!(Balances::free_balance(1), 100);
-    });
+    // failed on the second try
+    assert_noop!(
+      CloverClaims::claim(Origin::none(), 1, tx_hash.clone(), sig.clone()),
+      Error::<Test>::AlreadyClaimed
+    );
+    assert_eq!(Balances::free_balance(1), 100);
+  });
 }
 
 #[test]
 fn claim_limit_should_work() {
-    new_test_ext().execute_with(|| {
-        assert_ok!(CloverClaims::set_bridge_account(Origin::root(), 2));
+  new_test_ext().execute_with(|| {
+    assert_ok!(CloverClaims::set_bridge_account(Origin::root(), 2));
 
-        // Mint a claim should failed without limitation
-        let tx_hash = get_legal_tx_hash();
-        let eth_addr = get_legal_eth_addr();
-        assert_noop!(
-            CloverClaims::mint_claim(Origin::signed(2), tx_hash.clone(), eth_addr.clone(), 10),
-            Error::<Test>::ClaimLimitExceeded
-        );
+    // Mint a claim should failed without limitation
+    let tx_hash = get_legal_tx_hash();
+    let eth_addr = get_legal_eth_addr();
+    assert_noop!(
+      CloverClaims::mint_claim(Origin::signed(2), tx_hash.clone(), eth_addr.clone(), 10),
+      Error::<Test>::ClaimLimitExceeded
+    );
 
-        // Set limit
-        assert_ok!(CloverClaims::set_claim_limit(Origin::root(), 10));
-        assert_eq!(CloverClaims::claim_limit(), 10);
+    // Set limit
+    assert_ok!(CloverClaims::set_claim_limit(Origin::root(), 10));
+    assert_eq!(CloverClaims::claim_limit(), 10);
 
-        // Claim amount with limitation should be ok
-        assert_ok!(CloverClaims::mint_claim(Origin::signed(2), tx_hash.clone(), eth_addr.clone(), 10));
-        assert_eq!(CloverClaims::claim_limit(), 0);
-    });
+    // Claim amount with limitation should be ok
+    assert_ok!(CloverClaims::mint_claim(
+      Origin::signed(2),
+      tx_hash.clone(),
+      eth_addr.clone(),
+      10
+    ));
+    assert_eq!(CloverClaims::claim_limit(), 0);
+  });
 }
 
 #[test]
 fn burn_should_work() {
-    new_test_ext().execute_with(|| {
-        let eth_addr = get_legal_eth_addr();
+  new_test_ext().execute_with(|| {
+    let eth_addr = get_legal_eth_addr();
 
-        // should burn balance from account 4
-        assert_ok!(CloverClaims::burn(Origin::signed(4), eth_addr, 40));
-        assert_eq!(Balances::free_balance(4), 60);
+    // should burn balance from account 4
+    assert_ok!(CloverClaims::burn(Origin::signed(4), eth_addr, 40));
+    assert_eq!(Balances::free_balance(4), 60);
 
-        assert_ok!(CloverClaims::burn(Origin::signed(4), eth_addr, 40));
-        assert_eq!(Balances::free_balance(4), 20);
+    assert_ok!(CloverClaims::burn(Origin::signed(4), eth_addr, 40));
+    assert_eq!(Balances::free_balance(4), 20);
 
-        // should failed if burn all the balances from an account
-        assert_noop!(
-          CloverClaims::burn(Origin::signed(5), eth_addr, 100),
-          BalancesError::<Test, _>::KeepAlive);
-        assert_eq!(Balances::free_balance(5), 100);
-    });
+    // should failed if burn all the balances from an account
+    assert_noop!(
+      CloverClaims::burn(Origin::signed(5), eth_addr, 100),
+      BalancesError::<Test, _>::KeepAlive
+    );
+    assert_eq!(Balances::free_balance(5), 100);
+  });
+}
+
+#[test]
+fn burn_fee_should_work() {
+  new_test_ext().execute_with(|| {
+    assert_ok!(CloverClaims::set_burn_fee(Origin::root(), 30));
+    // set bridge account to root
+    assert_ok!(CloverClaims::set_bridge_account(Origin::root(), 1));
+    assert_ok!(CloverClaims::set_claim_limit(Origin::root(), 2000));
+
+    let eth_addr = get_legal_eth_addr();
+
+    // burn should failed if there is not enough balance to pay fee
+    assert_noop!(
+      CloverClaims::burn(Origin::signed(4), eth_addr, 20),
+      Error::<Test>::InvalidAmount
+    );
+    assert_eq!(Balances::free_balance(4), 100);
+
+    assert_ok!(CloverClaims::burn(Origin::signed(5), eth_addr, 40));
+    assert_eq!(Balances::free_balance(5), 60);
+
+    // module has the fee deposited
+    assert_eq!(Balances::free_balance(CloverClaims::account_id()), 30);
+  });
 }
