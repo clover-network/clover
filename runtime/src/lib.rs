234a22a5c34df14a630212363cfdc1ed3eb0dc7b
pangwa <pangwa@gmail.com>
2021-04-26 12:55:09 +0800

    adjust block gas limit

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -414,7 +414,7 @@ impl<F: FindAuthor<u32>> FindAuthor<H160> for EthereumFindAuthor<F> {
 }
 
 parameter_types! {
-  pub BlockGasLimit: U256 = U256::from(u32::max_value());
+  pub BlockGasLimit: U256 = U256::from(30_000_000); // double the ethereum block limit
 }
 
 impl pallet_ethereum::Config for Runtime {
