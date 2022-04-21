1b131c37ce07911336796f9e72322f36e964eb17
pangwa <pangwa@gmail.com>
2022-04-21 09:19:04 +0800

    implement buy_weight and refund_weight

--- runtime/src/asset_trader.rs
+++ runtime/src/asset_trader.rs
@@ -28,7 +28,7 @@ pub struct FungibleAssetTrader<
 	R: TakeRevenue,
 >(
 	Weight,
-	Option<(MultiLocation, u128)>,
+	Option<(MultiLocation, u128, u128)>,
 	PhantomData<(AssetId, LocationConverter, AssetWeightGetter, R)>,
 );
 
@@ -48,15 +48,85 @@ impl<
 		weight: Weight,
 		payment: xcm_executor::Assets,
 	) -> Result<xcm_executor::Assets, XcmError> {
-		Err(XcmError::TooExpensive)
+		let first_fungible = payment
+			.clone()
+			.fungible_assets_iter()
+			.next()
+			.ok_or(XcmError::TooExpensive)?;
+
+		match (first_fungible.id, first_fungible.fun) {
+			(xcmAssetId::Concrete(id), Fungibility::Fungible(_)) => {
+				let asset_id: AssetId =
+					LocationConverter::convert(id.clone()).map_err(|_| XcmError::AssetNotFound)?;
+				// we only support assets that have weight price configured
+				let units_per_second = AssetWeightGetter::get_units_per_second(asset_id)
+					.ok_or_else(|| XcmError::TooExpensive)?;
+				let amount = units_per_second.saturating_mul(weight as u128) / (WEIGHT_PER_SECOND as u128);
+				// the fee is too low
+				if amount.is_zero() {
+					return Ok(payment);
+				}
+				let fee = MultiAsset {
+					fun: Fungibility::Fungible(amount),
+					id: xcmAssetId::Concrete(id.clone()),
+				};
+				let unused = payment
+					.checked_sub(fee)
+					.map_err(|_| XcmError::TooExpensive)?;
+
+				self.0 = self.0.saturating_add(weight);
+				let new_info = match self.1.clone() {
+					Some((prev_loc, prev_amount, prev_units_per_second)) => {
+						if prev_loc == id.clone() {
+							// this should not happen really
+							if units_per_second != prev_units_per_second {
+								return Err(XcmError::TooExpensive);
+							}
+							// some payment happened previously and the payment asset is the same as this round,
+							// add the new amount to the payment
+							Some((id, prev_amount.saturating_add(amount), units_per_second))
+						} else {
+							// some payment made before, but it's not the same payment as this round
+							// ignore the refund for this round
+							None
+						}
+					}
+					None => Some((id, amount, units_per_second)),
+				};
+
+				if let Some(new_info) = new_info {
+					self.0 = self.0.saturating_add(weight);
+					self.1 = Some(new_info);
+				};
+
+				Ok(unused)
+			}
+			_ => Err(XcmError::TooExpensive),
+		}
 	}
 
 	fn refund_weight(&mut self, weight: Weight) -> Option<MultiAsset> {
-		None
+		if let Some((id, prev_amount, units_per_second)) = self.1.clone() {
+			// ensure we refund at most the weight we paid
+			let weight = weight.min(self.0);
+			self.0 -= weight;
+			let amount = units_per_second * (weight as u128) / (WEIGHT_PER_SECOND as u128);
+			self.1 = Some((
+				id.clone(),
+				prev_amount.saturating_sub(amount),
+				units_per_second,
+			));
+			Some(MultiAsset {
+				fun: Fungibility::Fungible(amount),
+				id: xcmAssetId::Concrete(id.clone()),
+			})
+		} else {
+			None
+		}
 	}
 }
 
-/// Deal with spent fees, deposit them as dictated by R
+/// helper to handle transaction fee payments
 impl<
 		AssetId: From<u64> + Clone,
 		LocationConverter: Convert<MultiLocation, AssetId>,
@@ -65,7 +135,7 @@ impl<
 	> Drop for FungibleAssetTrader<AssetId, LocationConverter, AssetWeightGetter, R>
 {
 	fn drop(&mut self) {
-		if let Some((id, amount)) = self.1.clone() {
+		if let Some((id, amount, _)) = self.1.clone() {
 			R::take_revenue((id, amount).into());
 		}
 	}
