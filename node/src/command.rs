d989a894d7bcdc0b8cb2005210dfab9eebf6553f
pangwa <pangwa@gmail.com>
2022-07-04 15:42:26 +0800

    default parachain id to 2002
    ease for testing

--- node/src/command.rs
+++ node/src/command.rs
@@ -77,7 +77,7 @@ impl SubstrateCli for Cli {
   }
 
   fn load_spec(&self, id: &str) -> std::result::Result<Box<dyn sc_service::ChainSpec>, String> {
-    load_spec(id, self.run.parachain_id.unwrap_or(100).into())
+    load_spec(id, self.run.parachain_id.unwrap_or(2002).into())
   }
 
   fn native_runtime_version(_: &Box<dyn ChainSpec>) -> &'static RuntimeVersion {
@@ -271,7 +271,7 @@ pub fn run() -> sc_cli::Result<()> {
             .chain(cli.relaychain_args.iter()),
         );
 
-        let id = ParaId::from(cli.run.parachain_id.or(para_id).unwrap_or(100));
+        let id = ParaId::from(cli.run.parachain_id.or(para_id).unwrap_or(2002));
 
         let parachain_account =
           AccountIdConversion::<polkadot_primitives::v0::AccountId>::into_account(&id);
