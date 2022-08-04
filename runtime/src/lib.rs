563f043cd0e8ee2e7f1b88616419872deb0bd03d
pangwa <pangwa@gmail.com>
2022-08-04 22:12:48 +0800

    bump 24

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -125,7 +125,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
   spec_name: create_runtime_str!("clover-mainnet"),
   impl_name: create_runtime_str!("clover-mainnet"),
   authoring_version: 1,
-  spec_version: 23,
+  spec_version: 24,
   impl_version: 1,
   apis: RUNTIME_API_VERSIONS,
   transaction_version: 1,
