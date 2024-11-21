446255a232b2b3bc1d1534767719b6af3509c98d
Dastanbek Samatov <dastanbeksamatov@gmail.com>
2024-11-21 18:27:32 +0600

    Downgrade toolchain as well

--- runtime/build.rs
+++ runtime/build.rs
@@ -1,9 +1,13 @@
-use substrate_wasm_builder::WasmBuilder;
+// use substrate_wasm_builder::WasmBuilder;
 
 fn main() {
-    WasmBuilder::new()
-        .with_current_project()
-        .export_heap_base()
-        .import_memory()
-        .build()
+    // WasmBuilder::new()
+    //     .with_current_project()
+    //     .export_heap_base()
+    //     .import_memory()
+    //     .build()
+    #[cfg(feature = "std")]
+    {
+        substrate_wasm_builder::WasmBuilder::build_using_defaults();
+    }
 }
