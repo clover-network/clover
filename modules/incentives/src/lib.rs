81b9a9aeeecb6860b044f9336d32d98271b816d6
Maxim Evtush <154841002+maximevtush@users.noreply.github.com>
2025-02-12 23:00:35 +0100

    Update lib.rs

--- modules/incentives/src/lib.rs
+++ modules/incentives/src/lib.rs
@@ -185,9 +185,9 @@ impl<T: Trait> IncentiveOps<T::AccountId, CurrencyId, Share, Balance> for Module
     }
   }
 
-  fn get_accumlated_rewards(who: &T::AccountId, left: &CurrencyId, right: &CurrencyId) -> Share {
+  fn get_accumulated_rewards(who: &T::AccountId, left: &CurrencyId, right: &CurrencyId) -> Share {
     if let Ok(id) = Self::get_dex_id(left, right) {
-      T::RewardPool::get_accumlated_rewards(who, &id)
+      T::RewardPool::get_accumulated_rewards(who, &id)
     } else {
       Zero::zero()
     }
@@ -196,10 +196,10 @@ impl<T: Trait> IncentiveOps<T::AccountId, CurrencyId, Share, Balance> for Module
   fn get_account_info(who: &T::AccountId, left: &CurrencyId, right: &CurrencyId) -> IncentivePoolAccountInfo<Share, Balance> {
     if let Ok(pool_id) = Self::get_dex_id(left, right) {
       let shares = T::RewardPool::get_account_shares(who, &pool_id);
-      let accumlated_rewards = T::RewardPool::get_accumlated_rewards(who, &pool_id);
-      IncentivePoolAccountInfo { shares, accumlated_rewards, }
+      let accumulated_rewards = T::RewardPool::get_accumulated_rewards(who, &pool_id);
+      IncentivePoolAccountInfo { shares, accumulated_rewards, }
     } else {
-      IncentivePoolAccountInfo { shares: Zero::zero(), accumlated_rewards: Zero::zero(), }
+      IncentivePoolAccountInfo { shares: Zero::zero(), accumulated_rewards: Zero::zero(), }
     }
   }
 
